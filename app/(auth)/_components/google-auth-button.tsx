8f1dd5b2cff3bdcaa02f9ee05503a24ea642ff12
Pavlo Myrskyi <pavlo.myrskyi@games>
2024-12-22 20:05:35 +0100

    api path updated

--- app/(auth)/_components/google-auth-button.tsx
+++ app/(auth)/_components/google-auth-button.tsx
@@ -3,12 +3,12 @@
 import { Button } from '@/components/ui/button';
 import { Icons } from '@/components/icons';
 import { toast } from 'sonner';
-import { API_BASE_URL } from '@/config/apiConfig';
+import { API_BASE_URL, CLIENT_URL } from '@/config/apiConfig';
 
 export default function GoogleSignInButton() {
   const handleGoogleLogin = async () => {
     try {
-      window.location.href = `${API_BASE_URL}/v1/auth/external-login/google?returnUrl=http://localhost:3000`;
+      window.location.href = `${API_BASE_URL}/v1/auth/external-login/google?returnUrl=${CLIENT_URL}`;
     } catch (error: any) {
       toast.error('Failed to initiate Google login.');
       console.error('Google login error:', error);
--- config/apiConfig.ts
+++ config/apiConfig.ts
@@ -1,5 +1,4 @@
-﻿// src/config/apiConfig.ts
-export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL ||
-  // 'https://roadmapsai.azurewebsites.net';
-  'https://localhost:5500';
+﻿export const API_BASE_URL =
+  process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:5500';
+export const CLIENT_URL =
+  process.env.NEXT_PUBLIC_CLIENT_URL || 'http://localhost:3000';
