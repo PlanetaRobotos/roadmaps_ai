825457dc9b0753cf513b9fa5cf3f4d5af174397f
Pavlo Myrskyi <pavlo.myrskyi@games>
2025-01-09 11:33:55 +0100

    clean code

--- app/(auth)/_components/callback.tsx
+++ app/(auth)/_components/callback.tsx
@@ -15,29 +15,29 @@ const AuthCallback: React.FC<AuthCallbackProps> = ({ redirectPath }) => {
   const router = useRouter();
   const searchParams = useSearchParams();
 
-  useEffect(() => {
-    console.log('searchParams:', searchParams);
-    const token = localStorage.getItem('token');
-
-    if (token) {
-      console.log('Token:', token);
-      login(token);
-
-      const params = new URLSearchParams(window.location.search);
-      params.delete('token');
-      const newUrl = `${window.location.pathname}?${params.toString()}`;
-      window.history.replaceState({}, '', newUrl);
-
-      toast.success('Successfully logged in!');
-      if (redirectPath != null) {
-        router.push(redirectPath);
-      }
-    }
-    //else {
-    //   toast.error('Authentication failed. No token found.');
-    //   router.push('/signin');
-    // }
-  }, [router, redirectPath]);
+  // useEffect(() => {
+  //   console.log('searchParams:', searchParams);
+  //   const token = localStorage.getItem('token');
+  //
+  //   if (token) {
+  //     console.log('Token:', token);
+  //     login(token);
+  //
+  //     const params = new URLSearchParams(window.location.search);
+  //     params.delete('token');
+  //     const newUrl = `${window.location.pathname}?${params.toString()}`;
+  //     window.history.replaceState({}, '', newUrl);
+  //
+  //     toast.success('Successfully logged in!');
+  //     if (redirectPath != null) {
+  //       router.push(redirectPath);
+  //     }
+  //   }
+  //   //else {
+  //   //   toast.error('Authentication failed. No token found.');
+  //   //   router.push('/signin');
+  //   // }
+  // }, [router, redirectPath]);
 
   return null;
 };
